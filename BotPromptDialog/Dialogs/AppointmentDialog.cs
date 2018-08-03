794718ac7112b408988b248ec2f8281d6c5d9889
Mnmartin <37138117+Mnmartin@users.noreply.github.com>
2018-08-03 13:19:56 +0300

    initial commit- bot basics

---
+++ BotPromptDialog/Dialogs/AppointmentDialog.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace vsmsbotv1.Dialogs
+{
+    [Serializable]
+    public class AppointmentDialog : IDialog<object>
+    {
+        string name;
+        string email;
+        string phone;
+        string company;
+        string plandetails;
+        
+        public AppointmentDialog(string plan)
+        {
+            plandetails = plan;
+        }
+
+        public AppointmentDialog()
+        {
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> activity)
+        {
+            var response = await activity;
+            if (response.Text.ToLower().Contains("yes"))
+            {
+                PromptDialog.Text(
+                    context: context,
+                    resume: ResumeGetName,
+                    prompt: "Please share your good name",
+                    retry: "Sorry, I didn't understand that. Please try again."
+                );
+            }
+            else
+            {
+                context.Done(this);
+            }
+            
+        }
+
+        public virtual async Task ResumeGetName(IDialogContext context, IAwaitable<string> Username)
+        {
+            string response = await Username;
+            name = response; ;
+
+            PromptDialog.Text(
+                context: context,
+                resume: ResumeGetEmail,
+                prompt: "Please share your Email ID",
+                retry: "Sorry, I didn't understand that. Please try again."
+            );
+        }
+
+        public virtual async Task ResumeGetEmail(IDialogContext context, IAwaitable<string> UserEmail)
+        {
+            string response = await UserEmail;
+            email = response; ;
+
+            PromptDialog.Text(
+                context: context,
+                resume: ResumeGetPhone,
+                prompt: "Please share your Mobile Number",
+                retry: "Sorry, I didn't understand that. Please try again."
+            );
+        }
+      
+
+        public virtual async Task ResumeGetPhone(IDialogContext context, IAwaitable<string> mobile)
+        {
+            string response = await mobile;
+            phone = response;
+
+            await context.PostAsync(String.Format("Hello {0} ,Congratulation :) your request has been sent Successfullly completed with Name = {0} Email = {1} Mobile Number = {2} . You will get Confirmation email and SMS", name, email, phone, company));
+
+            context.Done(this);
+        }
+    }
+}
---
+++ BotPromptDialog/Dialogs/RootDialog.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+
+namespace vsmsbotv1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            //Show the title with background image and Details
+            var message = context.MakeMessage();
+            var attachment = GetHeroCard();
+            message.Attachments.Add(attachment);
+            await context.PostAsync(message);
+
+            // Show the list of plan
+            context.Wait(MessageReceivedAsync);
+            //  return Task.CompletedTask;
+        }
+
+        private  async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            await context.PostAsync(" May I help you Request a meeting");
+            context.Call(new AppointmentDialog(),this.AppointmentDialogresumeafter);
+        }
+        private async Task AppointmentDialogresumeafter(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Wait(MessageReceivedAsync);
+
+        }
+
+        /// <summary>
+        /// Design Title with Image and About US link
+        /// </summary>
+        /// <returns></returns>
+        private static Attachment GetHeroCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "Visitor Security Management Service ",
+                Subtitle = "Request Meeting",
+                Text = "VSMS simplifies the Visitor Registration process by eliminating the mundane, unsecure aspects of how things are done today by incorporating the use of technology in efficient ways that enforces security, better communication, resulting in pleasant visitor experiences.",
+                Images = new List<CardImage> { new CardImage("https://sandbox.vsms.tezzasolutions.com/index.PNG") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Request meeting", value: "https://sandbox.vsms.tezzasolutions.com/") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+
+        public virtual async Task ChildDialogComplete(IDialogContext context, IAwaitable<object> response)
+        {
+            await context.PostAsync("Thanks for select C# Corner bot for Annual Conference 2018 Registrion .");
+            context.Done(this);
+        }
+
+       
+    }
+}
